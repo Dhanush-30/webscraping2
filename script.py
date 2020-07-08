dc189db35a12e6139a2e2be7f75a5e441672e942
Dhanush-30 <57610094+Dhanush-30@users.noreply.github.com>
2020-07-09 03:56:12 +0530

    Update script.py

--- script.py
+++ script.py
@@ -32,26 +32,40 @@ def scrap(field,Location,Experience):
             pass
     data1=pd.DataFrame(det)
     ##
-    urli='https://www.indeed.co.in/jobs?q='+str(field)+'&l='+str(Location)
+    
+    urli='https://www.indeed.co.in/jobs?q='+field+'&l='+Location
+
     driver.get(urli)
-    for i in range(7,12,2):
+
+    ik=driver.find_elements_by_tag_name('h2')
+    inlinks=[]
+    for i in range(len(ik)):
+        link=ik[i].find_element_by_tag_name('a').get_property('href')
+        inlinks.append(link)
+        length=len(inlinks)
+        if length==3:
+            break
+        else:
+            continue
+
+    detin=[]
+    for link in inlinks:
+        driver.get(link)
+        title=driver.find_element_by_tag_name('h3')
+        titlet=title.text
         try:
-            a=driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td/table/tbody/tr/td[1]/div['+str(i)+']/h2/a')
-            title=a.text
-        except:
-            pass
-        try:
-            company=driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td/table/tbody/tr/td[1]/div['+str(i)+']/div[1]/div[1]/span').text
+            company=driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[3]/div/div/div[1]/div[1]/div[2]/div[2]/div/div/div').text
         except:
             try:
-                company=driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td/table/tbody/tr/td[1]/div['+str(i)+']/div[1]/div[1]/span[1]').text
+                company=driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[3]/div/div/div[1]/div[1]/div[2]/div[4]/a/div/div/h4').text
             except:
-                company='Nan'
-        link=a.get_property('href')
-        tempj={'job_title':title,
+                company='not available right now or visit the link for updates'
+        
+        tempj={'job_title':titlet,
               'company':company,
               'link for more details':link}
         detin.append(tempj)
+
     datain=pd.DataFrame(detin)
     tdata=pd.concat([data1,datain])
     return tdata.to_html(header=True)
